ea48dc02b789b2c3cf4635c92c33d3025fd90654
sabbir240042147 <eftesabbir2725@gmail.com>
2026-05-17 14:54:33 +0600

    output test

---
+++ src/output/Myself.java
@@ -0,0 +1,8 @@
+package output;
+
+public class Myself {
+    public static void main(String[] args){
+        System.out.println("Sabbir");
+        System.out.println("Age:"+22);
+    }
+}
---
+++ src/output/Number.java
@@ -0,0 +1,11 @@
+package output;
+
+public class Number {
+    public static void main(String[] args){
+        System.out.println(1);
+        System.out.println(1+1);
+        System.out.println(3*1);
+        System.out.println(8/2);
+
+    }
+}
